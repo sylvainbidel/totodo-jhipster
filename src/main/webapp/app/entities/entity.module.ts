13b4006e63a8e369bfc95fa0bb3ff08a5b8db87a
Sylvain Bidel <bidel.sylvain@gmail.com>
2018-06-12 09:43:37 +0200

    cleanup

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { TodoTodoTheBibiLabModule } from './todo-the-bibi-lab/todo-the-bibi-lab.module';
+import { TodoPersonTheBibiLabModule } from './person-the-bibi-lab/person-the-bibi-lab.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        TodoTodoTheBibiLabModule,
+        TodoPersonTheBibiLabModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
